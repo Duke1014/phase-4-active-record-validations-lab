311bfe4abe7066c1a193628a4bb6e564f10a885b
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-11-29 12:58:30 -0500

    commit tree a4a2cbf036347b21e4005c310e84c66c2f28d9b4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_must_have_clickbait
+
+    def title_must_have_clickbait    
+        unless title.match?(/Won't Believe/)
+            errors.add(:title, "Must contain clickbait!") 
+        end
+    end
+    
 end
